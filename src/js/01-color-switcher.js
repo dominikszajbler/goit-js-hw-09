c3250fcca9ffb3e31c0d51a6a88400023dbd29a3
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-03-19 12:29:35 +0100

    color switcher ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,24 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
 const getRandomHexColor = () => {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  };
+  let randomHex;
+startBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    randomHex = setInterval(()=>{
+        body.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+})
+stopBtn.addEventListener("click",  () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    if(randomHex){
+        clearInterval(randomHex);
+    }
+});
 
-
-  
